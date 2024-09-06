7caf844d0ae07b7b78bec0a4bc9aaeaa36498c89
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-09-06 14:59:51 -0500

    updated bg style

--- src/components/courses/CoursesPage.jsx
+++ src/components/courses/CoursesPage.jsx
@@ -2,14 +2,6 @@ const CoursesPage = () => {
   return (
     <div className="min-h-screen dark:bg-black">
       <div class="relative overflow-hidden">
-        <div
-          aria-hidden="true"
-          class="flex absolute -top-96 start-1/2 transform -translate-x-1/2"
-        >
-          <div class="bg-gradient-to-r from-violet-300/50 to-purple-100 blur-3xl w-[25rem] h-[44rem] rotate-[-60deg] transform -translate-x-[10rem] dark:from-violet-900/50 dark:to-purple-900"></div>
-          <div class="bg-gradient-to-tl from-blue-50 via-blue-100 to-blue-50 blur-3xl w-[90rem] h-[50rem] rounded-fulls origin-top-left -rotate-12 -translate-x-[15rem] dark:from-indigo-900/70 dark:via-indigo-900/70 dark:to-blue-900/70"></div>
-        </div>
-
         <div class="relative z-10">
           <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 lg:py-16">
             <div class="max-w-2xl text-center mx-auto">
