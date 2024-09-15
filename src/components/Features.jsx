7fd54c1e6b9f7b3a7c7a09f4c4d43da2a281cd5b
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-09-15 09:01:06 -0500

    added html5 icon and updated card styling

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,94 +1,83 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPython } from "@fortawesome/free-brands-svg-icons";
 import { faJs } from "@fortawesome/free-brands-svg-icons";
-import { faJava } from "@fortawesome/free-brands-svg-icons";
+import { faHtml5 } from "@fortawesome/free-brands-svg-icons";
 
 const Features = () => {
   return (
     <div>
       <div class="max-w-[75rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-20 mx-auto">
-        <div class="grid sm:grid-cols-2 lg:grid-cols-4 items-center gap-12">
-          <div>
-            <FontAwesomeIcon
-              icon={faPython}
-              size="lg"
-              className="shrink-0 size-12 text-gray-800 dark:text-white"
-            />
-            <div class="bg-gradient-to-r from-gray-200 via-gray-50 to-transparent h-0.5 mt-6 dark:from-neutral-700 dark:via-neutral-900">
-              <div class="bg-gray-400 w-9 h-0.5 dark:bg-neutral-600"></div>
-            </div>
-            <div class="mt-5">
-              <h3 class="text-lg font-semibold text-gray-800 dark:text-white">
-                Python
-              </h3>
-              <p class="mt-1 text-gray-600 dark:text-neutral-400">
-                Responsive, and mobile-first project on the web
-              </p>
-            </div>
-          </div>
+        <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-6">
+          <a
+            class="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md focus:outline-none focus:shadow-md transition dark:bg-neutral-900 dark:border-neutral-800"
+            href="#"
+          >
+            <div class="p-4 md:p-5">
+              <div class="flex gap-x-5">
+                <FontAwesomeIcon
+                  icon={faPython}
+                  size="lg"
+                  className="shrink-0 size-12 text-gray-800 dark:text-white"
+                />
 
-          <div>
-            <FontAwesomeIcon
-              icon={faJs}
-              class="shrink-0 size-12 text-gray-800 dark:text-white"
-            ></FontAwesomeIcon>
-            <div class="bg-gradient-to-r from-gray-200 via-gray-50 to-transparent h-0.5 mt-6 dark:from-neutral-700 dark:via-neutral-900">
-              <div class="bg-gray-400 w-9 h-0.5 dark:bg-neutral-600"></div>
+                <div class="grow">
+                  <h3 class="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-neutral-400 dark:text-neutral-200">
+                    Learn Python &rarr;
+                  </h3>
+                  <p class="text-sm text-gray-500 dark:text-neutral-500">
+                    Get help from 40k+ Preline users
+                  </p>
+                </div>
+              </div>
             </div>
-            <div class="mt-5">
-              <h3 class="text-lg font-semibold text-gray-800 dark:text-white">
-                JavaScript
-              </h3>
-              <p class="mt-1 text-gray-600 dark:text-neutral-400">
-                Components are easily customized and extendable
-              </p>
+          </a>
+
+          <a
+            class="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md focus:outline-none focus:shadow-md transition dark:bg-neutral-900 dark:border-neutral-800"
+            href="#"
+          >
+            <div class="p-4 md:p-5">
+              <div class="flex gap-x-5">
+                <FontAwesomeIcon
+                  icon={faJs}
+                  class="shrink-0 size-12 text-gray-800 dark:text-white"
+                ></FontAwesomeIcon>
+
+                <div class="grow">
+                  <h3 class="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-neutral-400 dark:text-neutral-200">
+                    Learn JavaScript &rarr;
+                  </h3>
+                  <p class="text-sm text-gray-500 dark:text-neutral-500">
+                    Just head to «Help» in the app
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
-          <div>
-            <FontAwesomeIcon
-              icon={faJava}
-              class="shrink-0 size-12 text-gray-800 dark:text-white"
-            ></FontAwesomeIcon>
-            <div class="bg-gradient-to-r from-gray-200 via-gray-50 to-transparent h-0.5 mt-6 dark:from-neutral-700 dark:via-neutral-900">
-              <div class="bg-gray-400 w-9 h-0.5 dark:bg-neutral-600"></div>
+          </a>
+
+          <a
+            class="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md focus:outline-none focus:shadow-md transition dark:bg-neutral-900 dark:border-neutral-800"
+            href="#"
+          >
+            <div class="p-4 md:p-5">
+              <div class="flex gap-x-5">
+                <FontAwesomeIcon
+                  icon={faHtml5}
+                  size="lg"
+                  className="shrink-0 size-12 text-gray-800 dark:text-white"
+                />
+
+                <div class="grow">
+                  <h3 class="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-neutral-400 dark:text-neutral-200">
+                    Learn HTML &rarr;
+                  </h3>
+                  <p class="text-sm text-gray-500 dark:text-neutral-500">
+                    Reach us at{" "}
+                  </p>
+                </div>
+              </div>
             </div>
-            <div class="mt-5">
-              <h3 class="text-lg font-semibold text-gray-800 dark:text-white">
-                C++
-              </h3>
-              <p class="mt-1 text-gray-600 dark:text-neutral-400">
-                Every component and plugin is well documented
-              </p>
-            </div>
-          </div>
-          <div>
-            <svg
-              class="shrink-0 size-10 mt-8 text-gray-800 dark:text-white "
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path d="M14 9a2 2 0 0 1-2 2H6l-4 4V4c0-1.1.9-2 2-2h8a2 2 0 0 1 2 2v5Z" />
-              <path d="M18 9h2a2 2 0 0 1 2 2v11l-4-4h-6a2 2 0 0 1-2-2v-1" />
-            </svg>
-            <div class="bg-gradient-to-r from-gray-200 via-gray-50 to-transparent h-0.5 mt-6 dark:from-neutral-700 dark:via-neutral-900">
-              <div class="bg-gray-400 w-9 h-0.5 dark:bg-neutral-600"></div>
-            </div>
-            <div class="mt-5">
-              <h3 class="text-lg font-semibold text-gray-800 dark:text-white">
-                24/7 Support
-              </h3>
-              <p class="mt-1 text-gray-600 dark:text-neutral-400">
-                Contact us 24 hours a day, 7 days a week
-              </p>
-            </div>
-          </div>
+          </a>
         </div>
       </div>
     </div>
